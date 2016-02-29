6e4f1fa410394dff55e75cc6b19f13d68648b924
Franziska Schirrmacher <ef58ozyd@stud.informatik.uni-erlangen.de>
2016-02-29 13:06:54 +0100

    Allgemeine Struktur aufbauen mit projektion und rueckprojektion

--- src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/GaussianBlob.java
+++ src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/GaussianBlob.java
@@ -4,29 +4,37 @@ import ij.IJ;
 import ij.ImageJ;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
 
+
 public class GaussianBlob extends Grid2D {
 
 	protected double meanValueW;
 	protected double meanValueH;
 	protected double standardDeviationW;
 	protected double standardDeviationH;
-	protected double frequency;
 	
-	public GaussianBlob(int imageWidth, int imageHeight, double spacingWidth, double spacingHeight, double meanValueW,double meanValueH, double standardDeviationW,double standardDeviationH, double frequency){
+	
+	public GaussianBlob(int imageWidth, int imageHeight, double spacingWidth,
+			double spacingHeight, double meanValueW,double meanValueH, double standardDeviationW,double standardDeviationH){
 		
+		// create Image with the desired size
 		super(imageWidth, imageHeight);
 		this.setSpacing(spacingWidth, spacingHeight);
-		this.setOrigin((-((imageWidth-1)*spacingWidth)/2),(-((imageHeight-1)*spacingHeight)/2));
-		initialise(meanValueW,meanValueH, standardDeviationW,standardDeviationH, frequency);
+		this.setOrigin((-((imageWidth)*spacingWidth)/2),(-((imageHeight)*spacingHeight)/2));
+		// call Initialization function
+		initialise(meanValueW,meanValueH, standardDeviationW,standardDeviationH);
 	}
 	
-	private void initialise(double meanW,double meanH, double deviationW, double deviationH, double frequency){
+	// design Gaussian Blob with the given Parameters
+	private void initialise(double meanW,double meanH, double deviationW, double deviationH){
+		
+		//set Variables
 		this.meanValueW = meanW;
 		this.meanValueH = meanH;
 		this.standardDeviationW = deviationW;
 		this.standardDeviationH = deviationH;
-		this.frequency = frequency;
+		
 	
+		//Set Pixel Values of the Gaussian
 		for(int width = 0; width < this.getWidth(); width ++){
 			for(int height = 0; height < this.getHeight(); height ++){
 				
@@ -37,6 +45,7 @@ public class GaussianBlob extends Grid2D {
 		}	
 	}
 	
+	//Compute Value of the 2D Gaussian for the given Position
 	private float gaussian2D (double x, double y){
 		double exponent = -0.5*(Math.pow((x-this.meanValueW)/this.standardDeviationW, 2) + Math.pow((y-this.meanValueH)/this.standardDeviationH, 2));
 		double gaussianValue = Math.exp(exponent);
@@ -46,7 +55,32 @@ public class GaussianBlob extends Grid2D {
 	
 	public static void main(String[] args) {
 		new ImageJ();
-		GaussianBlob object = new GaussianBlob(512,512,1.0d,1.0d, 0.0d,0.0d,30.0d,30.0d,1.0);
-		object.show();
+		
+		int imageWidth = 512;
+		int imageHeight = 512;
+		double imageSpacingW = 1.0d;
+		double imageSpacingH = 1.0d;
+		double meanValueW = 0.0d;
+		double meanValueH = 0.0d;
+		double standardDeviationW = 30.d;
+		double standardDeviationH = 30.d;
+		
+		
+		int numberProjections = 379;
+		double detectorSpacing = 1.0d;
+		int numberPixel = 200;
+		
+		GaussianBlob object = new GaussianBlob(imageWidth,imageHeight,imageSpacingW,imageSpacingH,
+				meanValueW,meanValueH,standardDeviationW,standardDeviationH);
+		object.show("GaussianBlob");
+		
+		ParallelProjection sinogramm = new ParallelProjection(numberProjections,detectorSpacing,numberPixel);
+		sinogramm.createSinogramm(object);
+		sinogramm.show("Sinogramm");
+		
+		ParallelBackprojection image = new ParallelBackprojection(object);
+		image.filteredBackprojection(sinogramm, detectorSpacing, numberProjections);
+		image.show();
+
 	}
 }
--- src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/ParallelProjection.java
+++ src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/ParallelProjection.java
@@ -1,25 +1,113 @@
 package edu.stanford.rsl.tutorial.FranziFAU.ObjectMotion;
 
+import java.util.ArrayList;
+
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.InterpolationOperators;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.Box;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.PointND;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.StraightLine;
 
 public class ParallelProjection extends Grid2D{
 	
 	private int numberPixel;
-	private float detectorSize;
+	private double detectorSize;
 	private double detectorSpacing;
 	private double angleSpacing;
 	private int numberProjections;
 	
-	public ParallelProjection(int numberProjections, float detectorSpacing, int numberPixel){
+	public ParallelProjection(int numberProjections, double detectorSpacing, int numberPixel){
+		//create Grid2D for sinogramm
 		super(numberPixel, numberProjections);
 		
+		// initialize Variables with the given values
 		this.numberPixel = numberPixel;
 		this.detectorSize = numberPixel*detectorSpacing;
 		this.detectorSpacing = detectorSpacing;
 		this.angleSpacing = (Math.PI/numberProjections);
 		this.numberProjections = numberProjections;
+		
 		this.setOrigin(-detectorSize/2,0);
 		this.setSpacing(detectorSpacing,angleSpacing);
 	}
 	
+	public void createSinogramm(Grid2D phantom){
+		
+		Box imageBox = new Box(phantom.getWidth()*phantom.getSpacing()[0],phantom.getHeight()*phantom.getSpacing()[1],2.0d);
+		imageBox.setLowerCorner(new PointND(phantom.getOrigin()[0],-phantom.getOrigin()[1],-1.0));
+		imageBox.setUpperCorner(new PointND(-phantom.getOrigin()[0],phantom.getOrigin()[1],1.0));
+		
+		//walk over each projection
+		for(int indexProjections = 0; indexProjections < numberProjections; indexProjections++){
+			// walk along the detector
+			for(int indexDetektor = 0; indexDetektor < numberPixel; indexDetektor++){
+				
+				// define the parallel lines of the detector				
+				double [] indexWorld = this.indexToPhysical(indexDetektor, indexProjections);				
+				
+				double angle = angleSpacing * indexProjections;
+				double s = indexWorld[0];
+				double cos = Math.cos(angle);
+				double sin = Math.sin(angle);
+								
+								
+				PointND p1 = new PointND(cos*s, sin*s,0.0d);
+				PointND p2 = new PointND(cos*s - sin, sin*s + cos, 0.0d);
+				
+				
+				StraightLine line = new StraightLine(p1,p2);				
+				
+				
+				//intersection of the box and the line
+									
+				ArrayList<PointND> crossingPoints = imageBox.intersect(line);
+				
+				//if there is no intersection -> change direction of the line and look again
+				if(crossingPoints.size() == 0){
+					p2 = new PointND(cos*s + sin, sin*s - cos, 0.0d);
+					line = new StraightLine(p1,p2);
+					crossingPoints = imageBox.intersect(line);
+				}
+
+				if(crossingPoints.size() == 2){
+						PointND c1 = crossingPoints.get(0);						
+						PointND c2 = crossingPoints.get(1);					
+
+						//compute distance between intersectoin points
+						double distance = c1.euclideanDistance(c2);
+
+						// define the direction of the line
+						double deltax = (c2.get(0)-c1.get(0))/(distance);
+						double deltay = (c2.get(1)-c1.get(1))/(distance);
+						double deltaz = (c2.get(2)-c1.get(2))/(distance);				
+						
+						PointND richtung = new PointND(deltax,deltay,deltaz);
+						//result value at the current position in the sinogramm
+						float val = 0.f;
+						// stepsize of the integral
+						double delta = 0.5; // in mm
+						//line integral
+						for(double k = 0; k < (distance); k=k+delta){
+							
+							double indexX = c1.get(0) + k*(richtung.get(0));
+							double indexY = c1.get(1) + k*(richtung.get(1));							
+																					
+							double [] indexImage = phantom.physicalToIndex(indexX, indexY);			
+		
+							val += InterpolationOperators.interpolateLinear(phantom, indexImage[0], indexImage[1]);		
+
+						}
+						
+						this.setAtIndex(indexDetektor, indexProjections, (float)(val*delta));
+				}
+			
+			}
+		}
+		
+	
+		
+		return;
+	}
 }
+	
+
