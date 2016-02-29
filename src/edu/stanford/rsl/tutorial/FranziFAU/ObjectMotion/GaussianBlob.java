1b7e3f3121a5b97d8e5598f002205dedf3907497
Franziska Schirrmacher <ef58ozyd@stud.informatik.uni-erlangen.de>
2016-02-29 11:32:02 +0100

    Beginn Projektion

--- src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/GaussianBlob.java
+++ src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/GaussianBlob.java
@@ -17,7 +17,7 @@ public class GaussianBlob extends Grid2D {
 		super(imageWidth, imageHeight);
 		this.setSpacing(spacingWidth, spacingHeight);
 		this.setOrigin((-((imageWidth-1)*spacingWidth)/2),(-((imageHeight-1)*spacingHeight)/2));
-		this.initialise(meanValueW,meanValueH, standardDeviationW,standardDeviationH, frequency);
+		initialise(meanValueW,meanValueH, standardDeviationW,standardDeviationH, frequency);
 	}
 	
 	private void initialise(double meanW,double meanH, double deviationW, double deviationH, double frequency){
@@ -26,11 +26,27 @@ public class GaussianBlob extends Grid2D {
 		this.standardDeviationW = deviationW;
 		this.standardDeviationH = deviationH;
 		this.frequency = frequency;
+	
+		for(int width = 0; width < this.getWidth(); width ++){
+			for(int height = 0; height < this.getHeight(); height ++){
+				
+				double [] worldCoordinates = this.indexToPhysical(width, height);				
+				float value = gaussian2D(worldCoordinates[0], worldCoordinates[1]);				
+				this.setAtIndex(width, height, value);
+			}
+		}	
+	}
+	
+	private float gaussian2D (double x, double y){
+		double exponent = -0.5*(Math.pow((x-this.meanValueW)/this.standardDeviationW, 2) + Math.pow((y-this.meanValueH)/this.standardDeviationH, 2));
+		double gaussianValue = Math.exp(exponent);
+		gaussianValue += Math.sqrt(2*Math.PI)*this.standardDeviationW*this.standardDeviationH;
+		return (float)gaussianValue;
 	}
 	
 	public static void main(String[] args) {
 		new ImageJ();
-		GaussianBlob object = new GaussianBlob(512,512,1.0d,1.0d, 0.0d,0.0d,1.0d,1.0d,1.0);
+		GaussianBlob object = new GaussianBlob(512,512,1.0d,1.0d, 0.0d,0.0d,30.0d,30.0d,1.0);
 		object.show();
 	}
 }
