9837bfcd9dda20fe422fc666f1258cf6198be279
Franziska Schirrmacher <ef58ozyd@stud.informatik.uni-erlangen.de>
2016-06-01 16:57:26 +0200

    Simulation 4

--- src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/GaussianBlob.java
+++ src/edu/stanford/rsl/tutorial/FranziFAU/ObjectMotion/GaussianBlob.java
@@ -2,6 +2,10 @@ package edu.stanford.rsl.tutorial.FranziFAU.ObjectMotion;
 
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -59,64 +63,17 @@ public class GaussianBlob extends Grid2D {
 	
 	//Compute Value of the 2D Gaussian for the given position
 	private float gaussian2D (double x, double y, double [] mean, double [] deviation){		
-		
-		double exponent = -0.5*(Math.pow((x - mean[0])/deviation[0], 2) 
-				+ Math.pow((y - mean[1])/deviation[1], 2));
-		double gaussianValue = Math.exp(exponent);
-		gaussianValue /= Math.sqrt(2*Math.PI)*deviation[0]*deviation[1];
-		
-		return (float)gaussianValue;
-	}
 	
-	public static void gradProjectionGauss(NumericGrid gridRes, final NumericGrid grid, int value, boolean offsetleft) {
-		subtractOffset(gridRes,grid,grid,0,value,offsetleft);
-		gridRes.notifyAfterWrite();
-	}
-	
-	private static void subtractOffset(NumericGrid gridResult, final NumericGrid gridA, final NumericGrid gridB, int xOffset, int yOffset,boolean offsetleft) {
+		float circle = 0.0f;
+		
+		double left = Math.pow(x-mean[0],2) + Math.pow(y-mean[1], 2);
+		double right = Math.pow(deviation[0], 2);
+		if(left <= right){
+			circle = 1.0f;
+		}
+		
+		return circle;
 
-		if(gridA.getSize()[0] != gridB.getSize()[0] || gridA.getSize()[1] != gridB.getSize()[1] ){
-			System.err.println("Grids have different sizes so they can not be subtracted.");
-		}
-		
-		for (int x = xOffset; x < gridA.getSize()[0]+xOffset; ++x){
-			for (int y = yOffset; y < gridA.getSize()[1]+yOffset; ++y){
-				
-					
-					int xIdx = (x >= gridA.getSize()[0] || x < 0) ? Math.min(Math.max(0, x), gridA.getSize()[0]-1) : x;
-					int yIdx = (y >= gridA.getSize()[1] || y < 0) ? Math.min(Math.max(0, y), gridA.getSize()[1]-1) : y;
-					
-
-					if(offsetleft)
-						gridResult.setValue(gridA.getValue(new int[]{xIdx,yIdx}) - gridB.getValue(new int[]{x-xOffset,y-yOffset}),
-								new int[]{x-xOffset,y-yOffset});
-					else
-						gridResult.setValue(gridA.getValue(new int[]{x-xOffset,y-yOffset}) - gridB.getValue(new int[]{xIdx,yIdx}),
-								new int[]{x-xOffset,y-yOffset});
-				}
-		}
-		gridResult.notifyAfterWrite();
-	}
-	
-	public static float [] compareSinogrammsLinewise(Grid2D sino1, Grid2D sino2){
-		
-		NumericGridOperator op = new NumericGridOperator();
-		
-		if(sino1.getSize()[0] != sino2.getSize()[0] || sino1.getSize()[1] != sino2.getSize()[1] ){
-			System.err.println("Sinogramms have different sizes so they can not be subtracted.");
-		}		
-		
-		float [] result = new float[sino1.getHeight()];
-		
-		for(int projectionLine = 0; projectionLine < sino1.getHeight(); projectionLine++){
-			
-			Grid1D lineSino1 = sino1.getSubGrid(projectionLine);
-			Grid1D lineSino2 = sino2.getSubGrid(projectionLine);
-			
-			result[projectionLine] = op.weightedSSD(lineSino1, lineSino2, 1.0, 0.0);			
-		}
-		
-		return result;
 	}
 	
 	//computes the mean of an array
@@ -145,75 +102,17 @@ public class GaussianBlob extends Grid2D {
 		return variance;
 	}
 	
-	public static Grid2D filterSino(Grid2D sinogramm){
-		
-		Grid2D filteredSinogramm = new Grid2D(sinogramm);
-		//construction of the ramLak filter
-		Grid1DComplex filter = new Grid1DComplex(filteredSinogramm.getSubGrid(0));
-		
-		//watch out x-axes is from 0 to - infinity and then from infinity to 0!
-		for(int n = 0; n < filter.getSize()[0]/2; n++){
-			
-			float val;
-			if(n == 0){
-				val = (float)(1.f/(4.f*1.0d));
-				filter.setRealAtIndex(n, val);
-				filter.setImagAtIndex(n, 0.f);
-			}else if((n%2) == 0){
-				val = 0.f;
-			}else{
-				val = (-(1.f/(float)(Math.pow(n*1.0d, 2)*Math.pow(Math.PI, 2))));
-			}
-			
-			if(n != 0){
-				filter.setRealAtIndex(n, val);
-				filter.setImagAtIndex(n, 0.f);
-				filter.setRealAtIndex(filter.getSize()[0]-n, val);
-				filter.setImagAtIndex(filter.getSize()[0]-n, 0.f);
-			}
-		}
-		
-		filter.transformForward();				
-		
-		//read out each line of the sinogramm and apply filtering
-		for(int s = 0; s < sinogramm.getHeight(); s++){
-			
-			Grid1DComplex line = new Grid1DComplex(filteredSinogramm.getSubGrid(s));			
-			
-			//filter the line			
-			line.transformForward();
-			
-			// multiply line with filter
-			for(int index = 0; index < line.getSize()[0]; index++){
-				
-				Complex lineVal = new Complex(line.getRealAtIndex(index),line.getImagAtIndex(index));
-				Complex filterVal = new Complex(filter.getRealAtIndex(index),filter.getImagAtIndex(index));
-				
-				Complex result = lineVal.mul(filterVal);
-				line.setRealAtIndex(index, (float)result.getReal());
-				line.setImagAtIndex(index, (float)result.getImag());
-			}
 
-			// IFT of the detector line
-			line.transformInverse();
-				
-			//save filtered line
-			for(int indexWidth = 0; indexWidth < filteredSinogramm.getWidth(); indexWidth++){
-				filteredSinogramm.setAtIndex(indexWidth, s, line.getRealAtIndex(indexWidth));
-			}		
-		}
-		return filteredSinogramm;
-	}
 	
-	public static void main(String[] args)  {		
+	public static void main(String[] args) throws IOException  {		
 		
 		new ImageJ();
 		
 		//Loading Trajcectory
 		
-		String filename = "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Trajectory.tif";
-		Grid2D trajectory = ImageUtil.wrapImagePlus(IJ.openImage(filename)).getSubGrid(0);
-		trajectory.show("Trajectory");
+		String file = "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Trajectory2.tif";
+		Grid2D trajectoryImage = ImageUtil.wrapImagePlus(IJ.openImage(file)).getSubGrid(0);
+//		trajectoryImage.show("Trajectory");
 		
 		//Parameters for all methods
 	
@@ -225,7 +124,7 @@ public class GaussianBlob extends Grid2D {
 		int numberProjections = 2*180;
 		double detectorSpacing = 1.0d;
 		int numberPixel = 500;
-		double timeFactor = 23.0d/numberProjections; // time associated with one projection in seconds		
+		double timeFactor = 18.0d/numberProjections; // time associated with one projection in seconds		
 		
 		double frequency = 1.0d;	// in 1/second	
 		//
@@ -239,12 +138,12 @@ public class GaussianBlob extends Grid2D {
 		double [] changedMeanValue = {0.0d,0.0d};		
 		double [] changedStandardDeviation = {15.0d,15.0d};			
 				
-		//create GaussianBlob
+//		//create GaussianBlob
 		GaussianBlob object = new GaussianBlob(imageWidth, imageHeight, imageSpacing, meanValue, standardDeviation);
-		object.show("GaussianBlob");	
+//		object.show("GaussianBlob");	
 
 		GaussianBlob changedObject = new GaussianBlob(imageWidth, imageHeight, imageSpacing, changedMeanValue, changedStandardDeviation);
-		changedObject.show("changedGaussianBlob");
+//		changedObject.show("changedGaussianBlob");
 		
 		MovingGaussian movingObject = new MovingGaussian(imageWidth,imageHeight,
 				imageSpacing, meanValue, standardDeviation, frequency, changedMeanValue, changedStandardDeviation);
@@ -252,53 +151,44 @@ public class GaussianBlob extends Grid2D {
 		//create sinogramm of gaussianBlob
 		ParallelProjection sinogramm = new ParallelProjection(numberProjections, detectorSpacing, numberPixel, timeFactor);
 		Grid2D sino1 = sinogramm.createSinogrammMoving(movingObject);
-		sino1.show("Sinogramm");
+//		sino1.show("Sinogramm");
 		
 		//backproject sinogramm
 		ParallelBackprojection image = new ParallelBackprojection(object);
 		Grid2D reconstruct =  image.filteredBackprojection(sino1, detectorSpacing, numberProjections);
-		image.show("Backprojected image");				
+//		image.show("Backprojected image");				
 	
 		//create sinogramm of backprojected image
 		Grid2D sino2 = sinogramm.createSinogramm(reconstruct);
-		sino2.show("Sinogramm2");
+//		sino2.show("Sinogramm2");
 		
 		
 		//saving the images	
 
+		String title = "Pulsating";
 		
-//		String title = "Gaussian is pulsating";		
-//		Grid3D sino13D = new Grid3D(sino1.getWidth(),sino1.getHeight(),1);
-//		sino13D.setSpacing(sino1.getSpacing()[0],sino1.getSpacing()[1]);
-//		sino13D.setOrigin(sino1.getOrigin()[0],sino1.getOrigin()[1]);
-//		sino13D.setSubGrid(0, sino1);		
-//		ImagePlus imageSino1 = ImageUtil.wrapGrid3D(sino13D, title);		
-//		IJ.save(imageSino1, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation2/2.8_Sino1.tif");
-//		
-//		Grid3D back3D = new Grid3D(reconstruct.getWidth(),reconstruct.getHeight(),1);
-//		back3D.setSpacing(reconstruct.getSpacing()[0],reconstruct.getSpacing()[1]);
-//		back3D.setOrigin(reconstruct.getOrigin()[0],reconstruct.getOrigin()[1]);
-//		back3D.setSubGrid(0, reconstruct);		
-//		ImagePlus imageBack = ImageUtil.wrapGrid3D(back3D, title);		
-//		IJ.save(imageBack, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation2/2.8_Back.tif");
-//		
-//		Grid3D sino23D = new Grid3D(sino2.getWidth(),sino2.getHeight(),1);
-//		sino23D.setSpacing(sino2.getSpacing()[0],sino2.getSpacing()[1]);
-//		sino23D.setOrigin(sino2.getOrigin()[0],sino2.getOrigin()[1]);
-//		sino23D.setSubGrid(0, sino2);		
-//		ImagePlus imageSino2 = ImageUtil.wrapGrid3D(sino23D, title);		
-//		IJ.save(imageSino2, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation2/2.8_Sino2.tif");
+		ImagePlus imageSino1 = ImageUtil.wrapGrid(sino1, title);		
+		IJ.save(imageSino1, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation4/18 Perioden/1.7_Sino1.tif");
+		
+		
+		ImagePlus imageBack = ImageUtil.wrapGrid(reconstruct, title);		
+		IJ.save(imageBack, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation4/18 Perioden/1.7_Back.tif");
+		
+		
+		ImagePlus imageSino2 = ImageUtil.wrapGrid(sino2, title);		
+		IJ.save(imageSino2, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation4/18 Perioden/1.7_Sino2.tif");
 		
 		
 		//Gaussian Blob offset		
 		
 		double sigmaC = (standardDeviation[0] + changedStandardDeviation[0])/2;
-		double ln = sigmaC / standardDeviation[0];	
+//		double ln = sigmaC / standardDeviation[0];	
 			
 	
-		double shift = Math.sqrt(2) + Math.sqrt(2 - 2 * Math.log(ln));
-		shift *= sigmaC;
+
+		double shift = standardDeviation[0] - sigmaC;
 		System.out.println("shift: " + shift);
+
 		System.out.println("sigmaC: " + sigmaC);
 		
 		double [] meanValueOff = {0.0d, -shift};
@@ -307,10 +197,10 @@ public class GaussianBlob extends Grid2D {
 		double [] changedStandardDeviationOff = {sigmaC, sigmaC};
 
 		GaussianBlob objectOff = new GaussianBlob(imageWidth, imageHeight, imageSpacing, meanValueOff, standardDeviationOff);
-		objectOff.show("Off");
+//		objectOff.show("Off");
 		
 		GaussianBlob changedObjectOff = new GaussianBlob(imageWidth, imageHeight, imageSpacing, changedMeanValueOff, changedStandardDeviationOff);
-		changedObjectOff.show("Off2");
+//		changedObjectOff.show("Off2");
 		
 		MovingGaussian movingObjectOff = new MovingGaussian(imageWidth,imageHeight,
 				imageSpacing, meanValueOff, standardDeviationOff, frequency, changedMeanValueOff, changedStandardDeviationOff);		
@@ -318,52 +208,104 @@ public class GaussianBlob extends Grid2D {
 		//create sinogramm of gaussianBlob
 		ParallelProjection sinogrammOff = new ParallelProjection(numberProjections, detectorSpacing, numberPixel, timeFactor);
 		Grid2D sino1Off = sinogrammOff.createSinogrammMoving(movingObjectOff);
-		sino1Off.show("SinogrammOff");
+//		sino1Off.show("SinogrammOff");
 		
 		//backproject sinogramm
 		ParallelBackprojection imageOff = new ParallelBackprojection(objectOff);
 		Grid2D reconstructOff =  imageOff.filteredBackprojection(sino1Off, detectorSpacing, numberProjections);
-		imageOff.show("Backprojected imageOff");				
+//		imageOff.show("Backprojected imageOff");				
 	
 		//create sinogramm of backprojected image
-		Grid2D sino2Off = sinogramm.createSinogramm(reconstructOff);
-		sino2Off.show("Sinogramm2Off");		
+		Grid2D sino2Off = sinogrammOff.createSinogramm(reconstructOff);
+//		sino2Off.show("Sinogramm2Off");	
+		
+		String titleOff = "Offset";
+		
+		ImagePlus imageSino1Off = ImageUtil.wrapGrid(sino1Off, titleOff);		
+		IJ.save(imageSino1Off, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation4/18 Perioden/2.7_sino1.tif");
+		
+		
+		ImagePlus imageBackOff = ImageUtil.wrapGrid(reconstructOff, titleOff);		
+		IJ.save(imageBackOff, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation4/18 Perioden/2.7_back.tif");
+		
+		
+		ImagePlus imageSino2Off = ImageUtil.wrapGrid(sino2Off, title);		
+		IJ.save(imageSino2Off, "/home/cip/medtech2011/ef58ozyd/Projektarbeit/Simulation4/18 Perioden/2.7_sino2.tif");
 		
 		//Design trajectory
 		
-//		Grid2D trajectory = new Grid2D(imageWidth, imageHeight);
-//		trajectory.setSpacing(imageSpacing);
-//		trajectory.setOrigin((-((imageWidth)*imageSpacing[0])/2),(-((imageHeight)*imageSpacing[1])/2));		
-//		double [] idx = {0.0d,Math.sqrt(2)*sigmaC};
-//		double [] tmp = trajectory.physicalToIndex(idx[0], idx[1]);
+//		Grid2D singlePoint = new Grid2D(imageWidth, imageHeight);
+//		singlePoint.setSpacing(imageSpacing);
+//		singlePoint.setOrigin((-((imageWidth)*imageSpacing[0])/2),(-((imageHeight)*imageSpacing[1])/2));		
+//		double [] idx = {0.0d,sigmaC};
+//		double [] tmp = singlePoint.physicalToIndex(idx[0], idx[1]);
 //		int [] pixel = {(int)Math.floor(tmp[0]),(int)Math.floor(tmp[1])};
-//		trajectory.setValue(1.0f, pixel);		
-//		trajectory.show();			
+//		singlePoint.setValue(1.0f, pixel);		
+//		singlePoint.show();	
+//		
 //		ParallelProjection sinogrammT = new ParallelProjection(numberProjections, detectorSpacing, numberPixel, timeFactor);
-//		Grid2D traj = sinogrammT.createSinogramm(trajectory);
-//		traj.show("Result");		
-//		String titleTrajectory = "Trajectory";		
-//		Grid3D trajectory3D = new Grid3D(traj.getWidth(),traj.getHeight(),1);
-//		trajectory3D.setSpacing(traj.getSpacing()[0],traj.getSpacing()[1]);
-//		trajectory3D.setOrigin(traj.getOrigin()[0],traj.getOrigin()[1]);
-//		trajectory3D.setSubGrid(0, traj);
-//		ImagePlus trajectoryImg = ImageUtil.wrapGrid3D(trajectory3D, titleTrajectory);
-//		IJ.save(trajectoryImg,"/home/cip/medtech2011/ef58ozyd/Projektarbeit/Trajectory.tif");
+//		Grid2D trajectory = sinogrammT.createSinogramm(singlePoint);
+//		trajectory.show("Result");		
+//		String titleTrajectory = "Trajectory";	
+//		
+//		ImagePlus trajectoryImg = ImageUtil.wrapGrid(trajectory, titleTrajectory);
+//		IJ.save(trajectoryImg,"/home/cip/medtech2011/ef58ozyd/Projektarbeit/Trajectory2.tif");
 		
 		
 		float [] pulsating = new float[numberProjections];
 		float [] offset = new float[numberProjections];
 		
-		for(int x = 0; x < trajectory.getWidth(); x++){
-			for(int y = 0; y < trajectory.getHeight();y++){
-				if(trajectory.getAtIndex(x, y) != 0.f){
-					pulsating[y] = sinogramm.getAtIndex(x, y);
-					offset[y] = sinogrammOff.getAtIndex(x, y);
+		
+		for(int y = 0; y < trajectoryImage.getHeight();y++){
+			pulsating[y] = 0.f;
+			for(int x = 1; x < trajectoryImage.getWidth(); x++){			
+				if(trajectoryImage.getAtIndex(x, y) > trajectoryImage.getAtIndex(x-1, y)){
+					
+					pulsating[y] = sino2.getAtIndex(x, y);
+				
+				}
+				
+				if(trajectoryImage.getAtIndex(x, y) > trajectoryImage.getAtIndex(x-1, y)){				
+					
+					offset[y] = sino2Off.getAtIndex(x, y);
+					
 				}
 			}
 		}
-	
 		
+
+
+
+	
+		String filename = "Pulsating.txt";
+
+		BufferedWriter outputWriter = null;
+		outputWriter = new BufferedWriter(new FileWriter(filename));
+
+		for(int j = 0; j < numberProjections; j++){
+			String b = Double.toString(pulsating[j]);
+			outputWriter.write(b);
+			outputWriter.write(' ');
+			
+		}
+		outputWriter.close();
+		
+
+		
+		String filenameOff = "Offset.txt";
+		
+		BufferedWriter outputWriterOff = null;
+		outputWriterOff = new BufferedWriter(new FileWriter(filenameOff));
+		
+		
+		for(int j = 0; j < numberProjections; j++){
+			String Off = Double.toString(offset[j]);
+			outputWriterOff.write(Off);
+			outputWriterOff.write(' ');
+			
+			
+		}
+		outputWriterOff.close();
 		
 		System.out.print("Ende");
 		
--- src/edu/stanford/rsl/tutorial/FranziFAU/Test.java
+++ src/edu/stanford/rsl/tutorial/FranziFAU/Test.java
@@ -1,5 +1,10 @@
 package edu.stanford.rsl.tutorial.FranziFAU;
+import edu.stanford.rsl.conrad.data.generic.datatypes.Complex;
+import edu.stanford.rsl.conrad.data.numeric.Grid1D;
+import edu.stanford.rsl.conrad.data.numeric.Grid1DComplex;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.NumericGrid;
+import edu.stanford.rsl.conrad.data.numeric.NumericGridOperator;
 import edu.stanford.rsl.conrad.utils.ImageUtil;
 import edu.stanford.rsl.tutorial.FranziFAU.ObjectMotion.GaussianBlob;
 import ij.ImagePlus;
@@ -69,6 +74,131 @@ public class Test {
 		
 		
 	}
+	
+	
+		public static Grid2D filterSino(Grid2D sinogramm){
+		
+		Grid2D filteredSinogramm = new Grid2D(sinogramm);
+		//construction of the ramLak filter
+		Grid1DComplex filter = new Grid1DComplex(filteredSinogramm.getSubGrid(0));
+		
+		//watch out x-axes is from 0 to - infinity and then from infinity to 0!
+		for(int n = 0; n < filter.getSize()[0]/2; n++){
+			
+			float val;
+			if(n == 0){
+				val = (float)(1.f/(4.f*1.0d));
+				filter.setRealAtIndex(n, val);
+				filter.setImagAtIndex(n, 0.f);
+			}else if((n%2) == 0){
+				val = 0.f;
+			}else{
+				val = (-(1.f/(float)(Math.pow(n*1.0d, 2)*Math.pow(Math.PI, 2))));
+			}
+			
+			if(n != 0){
+				filter.setRealAtIndex(n, val);
+				filter.setImagAtIndex(n, 0.f);
+				filter.setRealAtIndex(filter.getSize()[0]-n, val);
+				filter.setImagAtIndex(filter.getSize()[0]-n, 0.f);
+			}
+		}
+		
+		filter.transformForward();				
+		
+		//read out each line of the sinogramm and apply filtering
+		for(int s = 0; s < sinogramm.getHeight(); s++){
+			
+			Grid1DComplex line = new Grid1DComplex(filteredSinogramm.getSubGrid(s));			
+			
+			//filter the line			
+			line.transformForward();
+			
+			// multiply line with filter
+			for(int index = 0; index < line.getSize()[0]; index++){
+				
+				Complex lineVal = new Complex(line.getRealAtIndex(index),line.getImagAtIndex(index));
+				Complex filterVal = new Complex(filter.getRealAtIndex(index),filter.getImagAtIndex(index));
+				
+				Complex result = lineVal.mul(filterVal);
+				line.setRealAtIndex(index, (float)result.getReal());
+				line.setImagAtIndex(index, (float)result.getImag());
+			}
+
+			// IFT of the detector line
+			line.transformInverse();
+				
+			//save filtered line
+			for(int indexWidth = 0; indexWidth < filteredSinogramm.getWidth(); indexWidth++){
+				filteredSinogramm.setAtIndex(indexWidth, s, line.getRealAtIndex(indexWidth));
+			}		
+		}
+		return filteredSinogramm;
+	}
+	
+		//Compute Value of the 2D Gaussian for the given position
+	private float gaussian2D (double x, double y, double [] mean, double [] deviation){		
+		
+		double exponent = -0.5*(Math.pow((x - mean[0])/deviation[0], 2) 
+				+ Math.pow((y - mean[1])/deviation[1], 2));
+		double gaussianValue = Math.exp(exponent);
+		gaussianValue /= Math.sqrt(2*Math.PI)*deviation[0]*deviation[1];
+		
+		return (float)gaussianValue;
+	}
+	
+	public static void gradProjectionGauss(NumericGrid gridRes, final NumericGrid grid, int value, boolean offsetleft) {
+		subtractOffset(gridRes,grid,grid,0,value,offsetleft);
+		gridRes.notifyAfterWrite();
+	}
+	
+	private static void subtractOffset(NumericGrid gridResult, final NumericGrid gridA, final NumericGrid gridB, int xOffset, int yOffset,boolean offsetleft) {
+
+		if(gridA.getSize()[0] != gridB.getSize()[0] || gridA.getSize()[1] != gridB.getSize()[1] ){
+			System.err.println("Grids have different sizes so they can not be subtracted.");
+		}
+		
+		for (int x = xOffset; x < gridA.getSize()[0]+xOffset; ++x){
+			for (int y = yOffset; y < gridA.getSize()[1]+yOffset; ++y){
+				
+					
+					int xIdx = (x >= gridA.getSize()[0] || x < 0) ? Math.min(Math.max(0, x), gridA.getSize()[0]-1) : x;
+					int yIdx = (y >= gridA.getSize()[1] || y < 0) ? Math.min(Math.max(0, y), gridA.getSize()[1]-1) : y;
+					
+
+					if(offsetleft)
+						gridResult.setValue(gridA.getValue(new int[]{xIdx,yIdx}) - gridB.getValue(new int[]{x-xOffset,y-yOffset}),
+								new int[]{x-xOffset,y-yOffset});
+					else
+						gridResult.setValue(gridA.getValue(new int[]{x-xOffset,y-yOffset}) - gridB.getValue(new int[]{xIdx,yIdx}),
+								new int[]{x-xOffset,y-yOffset});
+				}
+		}
+		gridResult.notifyAfterWrite();
+	}
+	
+	public static float [] compareSinogrammsLinewise(Grid2D sino1, Grid2D sino2){
+		
+		NumericGridOperator op = new NumericGridOperator();
+		
+		if(sino1.getSize()[0] != sino2.getSize()[0] || sino1.getSize()[1] != sino2.getSize()[1] ){
+			System.err.println("Sinogramms have different sizes so they can not be subtracted.");
+		}		
+		
+		float [] result = new float[sino1.getHeight()];
+		
+		for(int projectionLine = 0; projectionLine < sino1.getHeight(); projectionLine++){
+			
+			Grid1D lineSino1 = sino1.getSubGrid(projectionLine);
+			Grid1D lineSino2 = sino2.getSubGrid(projectionLine);
+			
+			result[projectionLine] = op.weightedSSD(lineSino1, lineSino2, 1.0, 0.0);			
+		}
+		
+		return result;
+	}
+	
+	
 	*/
 	}
 }
